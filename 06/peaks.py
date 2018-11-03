d526a4a7e1da9540671638ffccd6c61f3935fefb
Majo Durco <majodurco@gmail.com>
2018-11-03 22:57:52 +0100

    Add 06

---
+++ 06/peaks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import numpy
+import struct
+import sys
+import wave
+
+def main() :
+  numberOfArguments = 2;
+
+  if len(sys.argv) != numberOfArguments:
+    sys.exit('Wrong number of arguments')
+
+  _, waveFile = sys.argv
+
+  with wave.open(waveFile, 'r') as file:
+    freq = file.getframerate()
+    nframes = file.getnframes()
+    nchannels = file.getnchannels()
+    frames = file.readframes(freq)
+
+  lowest = numpy.inf
+  highest = -numpy.inf
+  for sample in range(int(nframes/freq)):
+    data = struct.unpack(str(freq * nchannels) + "h", frames)
+    data_numpy = numpy.array(data)
+    if nchannels == 2:
+      reshaped_data = data_numpy.reshape(-1, 2)
+      data_sum = reshaped_data.sum(axis=1) / 2
+
+    average = numpy.fft.rfft(data_sum) / freq
+    average_abs = numpy.abs(average)
+  
+    peaks = numpy.argwhere(average_abs >= 20*numpy.average(average_abs))
+    if peaks:
+      if peaks.min() < lowest: lowest = peaks.min()
+      if peaks.max() > highest: highest = peaks.max()
+
+  if numpy.isfinite(lowest) and numpy.isfinite(highest):
+    print('low = {}, high = {}'.format(lowest, highest))
+  else:
+    print('no peaks')
+
+if __name__ == '__main__':
+  main()
