78a9cb379346d2c68ccc213cebcacb3cc468750a
Majo Durco <majodurco@gmail.com>
2018-11-03 23:42:50 +0100

    Fix mutating data

--- 06/peaks.py
+++ 06/peaks.py
@@ -16,29 +16,27 @@ def main() :
     freq = file.getframerate()
     nframes = file.getnframes()
     nchannels = file.getnchannels()
-    frames = file.readframes(freq)
+    lowest = numpy.inf
+    highest = -numpy.inf
+    for sample in range(int(nframes/freq)):
+      frames = file.readframes(freq)
+      data = struct.unpack(str(freq * nchannels) + "h", frames)
+      data_numpy = numpy.array(data)
+      if nchannels == 2:
+        reshaped_data = data_numpy.reshape(-1, 2)
+        data_numpy = reshaped_data.sum(axis=1) / 2
 
-  lowest = numpy.inf
-  highest = -numpy.inf
-  for sample in range(int(nframes/freq)):
-    data = struct.unpack(str(freq * nchannels) + "h", frames)
-    data_numpy = numpy.array(data)
-    if nchannels == 2:
-      reshaped_data = data_numpy.reshape(-1, 2)
-      data_numpy = reshaped_data.sum(axis=1) / 2
-
-    average = numpy.fft.rfft(data_numpy) / freq
-    average_abs = numpy.abs(average)
-  
-    peaks = numpy.argwhere(average_abs >= 20*numpy.average(average_abs))
-    if len(peaks) > 0:
-      if peaks.min() < lowest: lowest = peaks.min()
-      if peaks.max() > highest: highest = peaks.max()
-
-  if numpy.isfinite(lowest) and numpy.isfinite(highest):
-    print('low = {}, high = {}'.format(lowest, highest))
-  else:
-    print('no peaks')
+      average = numpy.fft.rfft(data_numpy) / freq
+      average_abs = numpy.abs(average)
+    
+      peaks = numpy.argwhere(average_abs >= 20*numpy.average(average_abs))
+      if len(peaks) > 0:
+        if peaks.min() < lowest: lowest = peaks.min()
+        if peaks.max() > highest: highest = peaks.max()
+    if numpy.isfinite(lowest) and numpy.isfinite(highest):
+      print('low = {}, high = {}'.format(lowest, highest))
+    else:
+      print('no peaks')
 
 if __name__ == '__main__':
   main()
