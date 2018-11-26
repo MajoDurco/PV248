69c5cda9ce2fd9bbce16b324533a36415f07876a
Majo Durco <majodurco@gmail.com>
2018-11-26 22:22:07 +0100

    Add SSL to get

--- 09/http-forward.py
+++ 09/http-forward.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import re
+import ssl
 import sys
 import http.client
 import json
+import socket
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from OpenSSL.SSL import TLSv1_2_METHOD, Context, Connection
 
 def to_json(data):
     try:
@@ -21,19 +24,60 @@ def parseUrl(url):
     rest = '/' + split[1]
   else:
     rest = '/'
-  return (base, rest)
+  is_ssl = True if url.startswith('https://') else False
+  return (base, rest, is_ssl)
+
+def get_certificate_hosts(certificate):
+  hosts = ''
+  for extension_index in range(certificate.get_extension_count()):
+    extension = certificate.get_extension(extension_index)
+    if 'subjectAltName' in str(extension.get_short_name()):
+      hosts = str(extension)
+  hosts = hosts.split(', ')
+  return [host[4:] if host.startswith('DNS:') else host for host in hosts]
+
+def checkCertificate(url_base, url_rest, req_headers={}, request_type='GET'):
+  connection = http.client.HTTPSConnection(url_base, timeout=1)
+  certificate_is_valid = None
+  certificate_host_names = None
+  try:
+    client = socket.socket()
+    client.connect((url_base, 443))
+    client_ssl = Connection(Context(TLSv1_2_METHOD), client)
+    client_ssl.set_connect_state()
+    client_ssl.set_tlsext_host_name(url_base.encode('UTF-8'))
+    client_ssl.do_handshake()
+    certificate_host_names = get_certificate_hosts(client_ssl.get_peer_certificate())
+  except:
+    pass
+  finally:
+    if client:
+      client.close()
+  try:
+    connection.request(request_type, url_rest, headers=req_headers)
+    certificate_is_valid = True
+  except:
+    certificate_is_valid = False
+  return (certificate_is_valid, certificate_host_names)
+
 
 class ServerHandler(SimpleHTTPRequestHandler):
   def do_GET(self):
-    url_base, url_rest = parseUrl(url)
+    url_base, url_rest, is_ssl = parseUrl(url)
     response = {}
     try:
-      connection = http.client.HTTPSConnection(url_base, timeout=1)
-
       req_headers = dict(self.headers)
       if 'Host' in req_headers:
           del req_headers['Host']
+      cert_valid = None
+      cert_host_names = None
+      if is_ssl:
+        cert_valid, cert_host_names = checkCertificate(url_base, url_rest, req_headers)
 
+      context = ssl.create_default_context()
+      context.check_hostname = False
+      context.verify_mode = ssl.CERT_NONE
+      connection = http.client.HTTPSConnection(url_base, timeout=1, context=context)
       connection.request('GET', url_rest, headers=req_headers)
       response = connection.getresponse()
       headers = dict(response.getheaders())
@@ -47,7 +91,11 @@ class ServerHandler(SimpleHTTPRequestHandler):
         response['json'] = json_data
       else:
         response['content'] = data
-    except :
+      if cert_valid is not None:
+        response['certificate valid'] = cert_valid
+      if cert_host_names is not None:
+        response['certificate for'] = cert_host_names
+    except:
       response = { 'code': 'timeout' }
     finally:
       self.send_json(response)
@@ -71,7 +119,7 @@ class ServerHandler(SimpleHTTPRequestHandler):
       response['code'] = 'invalid json'
       return self.send_json(response)
 
-    url_base, url_rest = parseUrl(request_url)
+    url_base, url_rest, is_ssl = parseUrl(request_url)
     response = {}
     try:
       connection = http.client.HTTPSConnection(url_base, timeout=timeout)
