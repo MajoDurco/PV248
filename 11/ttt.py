2a086697301e950cba22a01b62f7721231a88b6c
Majo Durco <majodurco@gmail.com>
2018-12-11 22:37:57 +0100

    Finish server

--- 11/ttt.py
+++ 11/ttt.py
@@ -12,17 +12,14 @@ from urllib.parse import urlparse, parse_qs
 NOT_FINISHED = 0
 FINISHED = 1
 
-class WrongPlayerTurn(Exception):
+class GameException(Exception):
   pass
-class WrongCoordinates(Exception):
-  pass
-class BoardPlaceNotEmpty(Exception):
-  pass
-class GameIsFinishedCannotMove(Exception):
+class GameManagerException(Exception):
   pass
 
 class Game():
-  def __init__(self, name):
+  def __init__(self, game_id, name):
+    self.id = game_id
     self.name = name
     self.board = [
       [ 0, 0, 0],
@@ -73,13 +70,13 @@ class Game():
   
   def move(self, player_id, x, y):
     if self.status == FINISHED:
-      raise GameIsFinishedCannotMove('Game is finished cannot move')
+      raise GameException('Game is finished cannot move')
     if not self.is_players_turn(player_id):
-      raise WrongPlayerTurn('Not your turn')
+      raise GameException('Not your turn')
     if not self.are_coordinates_valid(x, y):
-      raise WrongCoordinates('Wrong coordinates')
+      raise GameException('Wrong coordinates')
     if not self.is_place_empty(x, y):
-      raise BoardPlaceNotEmpty('Place alredy taken')
+      raise GameException('Place alredy taken')
     
     self.board[x][y] = self.player_turn
     if self.is_move_victory(x, y):
@@ -94,83 +91,147 @@ class Game():
 class GamesManager():
   def __init__(self):
     self.games = {}
-    self.next_id = 0
+    self.next_game_id = 0
 
-  def createGame(self, name):
-    game = Game(name)
-    self.games[self.next_id] = game
-    self.next_id += 1
+  def create_game(self, name):
+    game = Game(self.next_game_id, name)
+    self.games[self.next_game_id] = game
+    self.next_game_id += 1
     return game
+  
+  def get_game(self, game_id):
+    if not game_id in self.games:
+      raise GameManagerException('Game does not exist')
+    return self.games[game_id]
+
+  def get_game_status(self, game_id):
+    game = self.get_game(game_id)
+    if game.status == FINISHED:
+      return {
+        'winner': game.winner
+      }
+    else:
+      return {
+        'board': game.board,
+        'next': game.get_next_turn_player(),
+      }
 
 class Handler(BaseHTTPRequestHandler):
-    pass
-    # def do_GET(self):
-    #     print(self.path)
-    #     parsed = urlparse.urlsplit(self.path)
+  def do_HEAD(self):
+    self.send_error(405, 'HEAD not supported')
+    
+  def do_POST(self):
+    self.send_error(405, 'GET not supported')
 
-    #     action = parsed.path[1:]
-    #     params = urlparse.parse_qs(parsed.query)
-    #     if action == 'start':
-    #         name = params['name'][0] if 'name' in params else ''
-    #         return self.start_game(name)
-    #     elif action == 'status':
-    #         if 'game' not in params:
-    #             return self.invalid_request(
-    #                 code=400,
-    #                 status='bad',
-    #                 message='Missing parameter: game')
-    #         try:
-    #             game_id = int(params['game'][0])
-    #         except ValueError:
-    #             return self.invalid_request(
-    #                 code=400,
-    #                 status='bad',
-    #                 message='Wrong parameter (non-integer): game'
-    #             )
-    #         return self.state_of_game(game_id)
-    #     elif action == 'play':
-    #         if 'game' not in params or \
-    #             'player' not in params or \
-    #             'x' not in params or \
-    #             'y' not in params:
-    #             return self.invalid_request(
-    #                 code=400,
-    #                 status='bad',
-    #                 message='Missing parameter'
-    #             )
-    #         try:
-    #             game_id = int(params['game'][0])
-    #             player_id = int(params['player'][0])
-    #             x = int(params['x'][0])
-    #             y = int(params['y'][0])
-    #         except ValueError:
-    #             return self.invalid_request(
-    #                 code=400,
-    #                 status='bad',
-    #                 message='Wrong parameter (non-integer)'
-    #             )
-    #         return self.play_turn(game_id, player_id, x, y)
-    #     else:
-    #         return self.invalid_request(
-    #             code=400,
-    #             status='bad',
-    #             message='Invalid action requested (start|status|play)'
-    #         )
+  def set_headers(self, content_len):
+    for keyword, value in self.headers.items():
+        self.send_header(keyword, value)
+    self.send_header('Content-Length', content_len)
+    self.end_headers()
+  
+  def handle_start(self):
+    name = parse_qs(self.parsed.query).get('name')
+    new_game = None
+    if name is None:
+      new_game = gameManager.create_game('')
+    else:
+      new_game = gameManager.create_game(name[0])
+    self.send_result(200, {
+      'id': str(new_game.id)
+    })
 
-# class ThreadingServer(ThreadingMixIn, HTTPServer):
-#   pass
+  def handle_play(self):
+    params = parse_qs(self.parsed.query)
+    game = params.get('game')
+    player = params.get('player')
+    x = params.get('x')
+    y = params.get('y')
+    if game is None or player is None or x is None or y is None:
+      self.send_result(400, {
+        'status': 'bad',
+        'message': 'Wrong query arguments for /play'
+      })
+    try:
+      game = int(game[0])
+      player = int(player[0])
+      x = int(x[0])
+      y = int(y[0])
+    except ValueError:
+      self.send_result(400, {
+        'status': 'bad',
+        'message': 'All query parameters should be integers'
+      })
+    try:
+      game = gameManager.get_game(game)
+      game.move(player, x, y)
+      self.send_result(200, { 'status': 'ok' })
+    except (GameManagerException, GameException) as err:
+      self.send_result(409, {
+        'status': 'bad',
+        'message': str(err)
+      })
 
-# def main():
-#   with ThreadingServer(('', int(port)), Handler) as httpd:
-#     httpd.serve_forever()
+  def handle_status(self):
+    game = parse_qs(self.parsed.query).get('game')
+    if game is None:
+      self.send_result(400, {
+        'status': 'bad',
+        'message': 'Game id is missing'
+      })
+    try:
+        game = int(game[0])
+    except ValueError:
+      self.send_result(400, {
+        'status': 'bad',
+        'message': 'All query parameters should be integers'
+      })
+    try:
+      status = gameManager.get_game_status(game)
+      self.send_result(200, status)
+    except (GameManagerException, GameException) as err:
+      self.send_result(409, {
+        'status': 'bad',
+        'message': str(err)
+      })
 
-# if __name__ == '__main__':
-#   number_of_arguments = 2;
+  def do_GET(self):
+    self.output = {}
+    self.parsed = urlparse(self.path)
+    print(self.parsed)
+    path = self.parsed.path
+    if path == '/start':
+      self.handle_start()
+    elif path == '/play':
+      self.handle_play()
+    elif path == '/status':
+      self.handle_status()
+    else:
+      self.send_error(404, 'Path not found')
+      return
 
-#   if len(sys.argv) != number_of_arguments:
-#     sys.exit('Wrong number of arguments')
+  def send_result(self, status_code, data):
+    data = json.dumps(data)
+    self.send_response(status_code)
+    self.send_header('Content-Type', 'application/json')
+    self.send_header('Content-Length', str(len(data)))
+    self.end_headers()
 
-#   _, port = sys.argv
-#   main()
+    self.wfile.write(bytes(data, 'UTF-8'))
 
-games = GamesManager()
+class ThreadingServer(ThreadingMixIn, HTTPServer):
+  pass
+
+def main():
+  with ThreadingServer(('', int(port)), Handler) as httpd:
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+  number_of_arguments = 2;
+
+  if len(sys.argv) != number_of_arguments:
+    sys.exit('Wrong number of arguments')
+
+  _, port = sys.argv
+  gameManager = GamesManager()
+
+  main()
